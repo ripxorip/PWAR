8d72c4704a0494c6da63384389fd22cf1b66da32
Philip K. Gisslow <ripxorip@gmail.com>
2025-05-24 21:43:08 +0200

    feat: First Linux test code

---
+++ linux/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake for building pwar for Linux";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
+    nix-formatter-pack.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , nix-formatter-pack
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+
+    let
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ ];
+      };
+
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gcc
+          pkg-config
+          pipewire.dev
+        ];
+      };
+    });
+}
